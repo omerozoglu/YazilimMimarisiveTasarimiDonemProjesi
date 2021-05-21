20b1a399362b908da05df757bb6b9f90aa7a7587
omerozoglu <omerozoglu.1@gmail.com>
2021-05-21 16:37:28 +0300

    :: Install MongoDB Driver , Domain, ++Food {dotnet add package MongoDB.Driver --version 2.12.3}

--- ProjectDietician.API/Domain/Entities/Diets/Food.cs
+++ ProjectDietician.API/Domain/Entities/Diets/Food.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Domain.Common;
 
 namespace Domain.Entities.Diets {
@@ -5,5 +6,8 @@ namespace Domain.Entities.Diets {
 
         //*string Name: Gıdanın Adı Örnek Sebze, meyve, tahıl,..vb
         public string Name { get; set; }
+
+        //* Hastalık ve diet yöntemi adları sadece string olarak tutulacak
+        public List<string> Tag { get; set; }
     }
 }
